c9b3accfa3e83094c80af1ababff917b881cbbed
fo0man <jmatty1983@gmail.com>
2015-02-06 19:18:20 +0000

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
+  root 'static_pages/home'
+  get 'static_pages/about'
   get 'static_pages/help'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'application#hello'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
