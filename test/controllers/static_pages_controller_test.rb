50608b8d544b3159571cd99bc20c29b4d433b35e
fo0man <jmatty1983@gmail.com>
2015-02-07 16:26:12 +0000

    Add sass to custom css

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -18,4 +18,6 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
     assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
+  
+  
 end
