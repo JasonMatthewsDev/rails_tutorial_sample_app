e0a3836051dcbec421f298c97da245e675c229b4
fo0man <jmatty1983@gmail.com>
2015-02-16 17:19:07 +0000

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
+  
   attr_accessor :remember_token
+  
   before_save { self.email = email.downcase }
+  
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
@@ -34,5 +38,9 @@ class User < ActiveRecord::Base
   def authenticated?(remember_token)
     return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+  
+  def feed
+    Micropost.where("user_id = ?", id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   post    'login'   => 'sessions#create'
   delete  'logout'  => 'sessions#destroy'
   resources :users
+  resources :microposts, only: [:create, :destroy]
 end
