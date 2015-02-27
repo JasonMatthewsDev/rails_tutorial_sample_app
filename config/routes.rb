4404487a90a291c647dc27bb5ab49616573c6733
fo0man <jmatty1983@gmail.com>
2015-02-27 17:54:19 +0000

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,13 @@ Rails.application.routes.draw do
   get     'login'   => 'sessions#new'
   post    'login'   => 'sessions#create'
   delete  'logout'  => 'sessions#destroy'
-  resources :users
-  resources :microposts, only: [:create, :destroy]
+
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+
+  resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
 end
